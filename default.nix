55036110019c177b0fc8dc9b2a20a2ca7c43b9d1
S. M. Hickman <sean@grump.ly>
2018-05-14 19:07:20 -0500

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ mkDerivation, base, containers, pure-core, pure-default
+, pure-lifted, pure-txt, stdenv
+}:
+mkDerivation {
+  pname = "pure-events";
+  version = "0.7.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    base containers pure-core pure-default pure-lifted pure-txt
+  ];
+  homepage = "github.com/grumply/pure-events";
+  license = stdenv.lib.licenses.bsd3;
+}
